dd63686f81ffa22eeb41f742a235eb8e5f8a2d77
yui <144975180+yuiwuulab@users.noreply.github.com>
2023-12-27 01:15:02 +0800

    manager dessert index
    新增一些按鈕

--- manager_dessert_index.php
+++ manager_dessert_index.php
@@ -1,6 +1,6 @@
 <?php
 // 載入db.php來連結資料庫
-require_once 'db.php';
+require_once '../db.php';
 //session_start();
 ?>
 <!DOCTYPE html>
@@ -13,8 +13,8 @@ require_once 'db.php';
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
-    <script src="all.js"></script>
-    <link rel="stylesheet" href="all.css">
+    <script src="../js/all.js"></script>
+    <link rel="stylesheet" href="../css/all.css">
 </head>
 
 <body>
@@ -22,17 +22,25 @@ require_once 'db.php';
         <div class="navbar">
             <?php
             if (isset($_SESSION['nowUser']) && $_SESSION['nowUser']['user_Role'] == "manager") {
-                echo "<h1 class='logo'><a href='manager_index.php'>搜蒐甜點店</a></h1>";
+                echo "<h1 class='logo'><a href='../manager_index.php'>搜蒐甜點店</a></h1>";
             } else {
-                echo "<h1 class='logo'><a href='index.php'>搜蒐甜點店</a></h1>";
+                echo "<h1 class='logo'><a href='../index.php'>搜蒐甜點店</a></h1>";
             }
             ?>
-            <form action="index-select.php" method="GET" class="search-section">
+            <form action="../shop/select.php" method="GET" class="search-section">
                 <input type="button" name="zone-choice" id="index-zone-choice" value="選擇地區">
                 <ul class="zone-choice-dropdown">
-                    <li><a href="index-select.php?zone=中壢區">中壢區</a></li>
-                    <li><a href="index-select.php?zone=桃園區">桃園區</a></li>
-                </ul>
+                        <?php 
+                            $zoneSql="SELECT DISTINCT(SUBSTRING(shop_Address, LOCATE('桃園市', shop_Address) + CHAR_LENGTH('桃園市'), 3)) AS district FROM shop HAVING district LIKE '%區'";
+                            $zone_result = $conn->query($zoneSql);
+                            if ($zone_result->num_rows > 0) {
+                                while ($zone_row = $zone_result->fetch_assoc()) {
+                                    echo "<li class='zone'><a href='../shop/select.php?zone-choice=".$zone_row['district']."'>".$zone_row['district']."</a></li>";
+                                }
+                            }
+                        ?>
+                        
+                    </ul>
                 <input type="button" name="style-choice" id="index-style-choice" value="選擇種類">
                 <ul class="style-choice-dropdown">
                     <?php //更改處
@@ -41,7 +49,7 @@ require_once 'db.php';
                     if ($result->num_rows > 0) {
                         while ($row = $result->fetch_assoc()) {
                             // Shop is visited
-                            echo "<li class='style' name='dess-type' id='dess-type'><a href='index-select.php?type=" . $row['desstype_Name'] . "' id='dess-type' >" . $row['desstype_Name'] . "</a></li>";
+                            echo "<li class='style' name='dess-type' id='dess-type'><a href='../shop/select.php?type=" . $row['desstype_Name'] . "' id='dess-type' >" . $row['desstype_Name'] . "</a></li>";
                         }
                     }
                     ?>
@@ -64,16 +72,16 @@ require_once 'db.php';
                 if (isset($_SESSION['nowUser'])) {
                     // 使用者已登入，顯示收藏和圖鑑
                     if ($_SESSION['nowUser']['user_Role'] == "manager") {
-                        echo '<li class="nav-content"><a href="manager_index.php">管理<br>店家</a></li>';
+                        echo '<li class="nav-content"><a href="../manager_index.php">管理店家</a></li>';
                     }
-                    echo '<li class="nav-content"><a href="favorite.php?userid=' . $_SESSION['nowUser']['user_ID'] . '">收藏</a></li>';
-                    echo '<li class="nav-content"><a href="gallery.php?userid=' . $_SESSION['nowUser']['user_ID'] . '">圖鑑</a></li>';
-                    echo '<li class="nav-content"><a href="user_info.php?userid=' . $_SESSION['nowUser']['user_ID'] . '"><i class="fa-solid fa-user"></i></a></li>';
+                    echo '<li class="nav-content"><a href="../favorite/favorite.php?userid=' . $_SESSION['nowUser']['user_ID'] . '">收藏</a></li>';
+                    echo '<li class="nav-content"><a href="../gallery/gallery.php?userid=' . $_SESSION['nowUser']['user_ID'] . '">圖鑑</a></li>';
+                    echo '<li class="nav-content"><a href="../user/user_info.php?userid=' . $_SESSION['nowUser']['user_ID'] . '"><i class="fa-solid fa-user"></i></a></li>';
                 } else {
                     // 使用者未登入
                     echo '<li class="nav-content hide"><a href="#">收藏</a></li>';
                     echo '<li class="nav-content hide"><a href="#">圖鑑</a></li>';
-                    echo '<li class="nav-content"><a href="signup.php"><i class="fa-solid fa-user"></i></a></li>';
+                    echo '<li class="nav-content"><a href="../user/signup.php"><i class="fa-solid fa-user"></i></a></li>';
                 }
                 ?>
             </ul>
@@ -95,7 +103,7 @@ require_once 'db.php';
                 $result = $conn->query($sql);
                 if ($result->num_rows > 0) {
                     // 顯示資料表格
-                    echo "<table id='dessert-table'><tr><th>ID</th><th>名稱</th><th>價格</th><th>類型</th><th> </th></tr>";
+                    echo "<table id='dessert-table'><tr><th>ID</th><th>名稱</th><th>價格</th><th>類型</th><th></th><th></th></tr>";
                     while ($row = $result->fetch_assoc()) {
                         $changed_shop_ID = $row["shop_ID"];
                         $changed_dess_ID = $row["dess_ID"];
@@ -109,21 +117,23 @@ require_once 'db.php';
                         echo "</tr>";
                     }
                     echo "</table>";
-                    echo "<button onclick=\"addRow('$shopID')\">新增</button>";
+                    echo "<button onclick=\"addRow('$shopID')\" class='add-button'>新增</button>";
+
                 } else {
                     echo "<div class='noDess'>0 筆結果</div><br>";
-                    echo "<table id='dessert-table'><tr><th>ID</th><th>名稱</th><th>價格</th><th>類型</th><th> </th></tr>";
+                    echo "<table id='dessert-table'><tr><th>ID</th><th>名稱</th><th>價格</th><th>類型</th><th></th><th></th></tr>";
                     echo "</table>";
-                    echo "<button onclick=\"addRow('$shopID')\">新增</button>";
+                    echo "<button onclick=\"addRow('$shopID')\" class='add-button'>新增</button>";
                 }
-
+                echo "<div style='display: flex;justify-content: flex-end;'><a href='../manager_index.php'><button class='add-button' >回到目錄</button></a>";
+                echo "<a href='../shop/shop_info.php?shop_id=$shopID'><button class='add-button'>前往店家</button></a></div>";
                 $conn->close();
                 ?>
             </div>
         </div>
     </div>
     </div>
-    <div class="footer">
+    <div class="footer" >
         <div class="left-footer"><img src='../image/logo-4.png'></div>
         <div class="right-footer">
             <p>Copyright © 2023 搜蒐甜點店 All Rights Reserved</p>
